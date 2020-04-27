1de198c9e38860c5eb65de38a773234a656baa1d
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-04-26 19:01:05 -0700

    Made Scripts for Mushrooms
    Not added to the game yet

---
+++ Super UAT Brothers/Assets/Scripts/CollectibleKeep.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleKeep : MonoBehaviour
+{
+    public GameObject regularMushroom;
+    public GameObject rareMushroom;
+    public static int regShroomCount;
+    public static int rareShroomCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Super UAT Brothers/Assets/Scripts/RegularMush.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularMush : MonoBehaviour
+{
+    public GameObject mushroom;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == collision)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+            CollectibleKeep.regShroomCount = +1;
+        }
+    }
+}
